b3d7463c6c32e3377703155190641639404bdbbb
Darlington Olelewe <darlington.olelewe@decagon.dev>
2022-04-13 22:05:17 +0100

    first commit

---
+++ src/main/java/com/brainf__k/tracktodo/TrackTodoApplication.java
@@ -0,0 +1,13 @@
+package com.brainf__k.tracktodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackTodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brainf__k/tracktodo/TrackTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brainf__k.tracktodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
