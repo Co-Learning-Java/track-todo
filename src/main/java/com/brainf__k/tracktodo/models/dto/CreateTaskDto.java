0fd0ba3286634d6d375c9f315d1bfe815ddf2ef5
user <obidikewinifred@yahoo.com>
2022-04-16 15:54:51 +0100

    created dto classes

--- src/main/java/com/brainf__k/tracktodo/models/dto/CreateTaskDto.java
+++ src/main/java/com/brainf__k/tracktodo/models/dto/CreateTaskDto.java
@@ -1,4 +1,7 @@
 package com.brainf__k.tracktodo.models.dto;
 
 public class CreateTaskDto {
+    private String title;
+    private String description;
+    private String dueDate;
 }
--- src/main/java/com/brainf__k/tracktodo/models/dto/EditTaskDto.java
+++ src/main/java/com/brainf__k/tracktodo/models/dto/EditTaskDto.java
@@ -1,4 +1,7 @@
 package com.brainf__k.tracktodo.models.dto;
 
 public class EditTaskDto {
+    private String title;
+    private String description;
+    private String status;
 }
